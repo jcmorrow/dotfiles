83c5b0dc6332c8602f03a6b2bd5c744d0758ea59
Josh Morrow <josh@jcmorrow.com>
2018-07-16 00:10:28 +0200

    Stealing more stuff from Gabe and removing oh-my-zsh
    WIP
    
    Ugh. IDK
    
    And add fuzzy finding git branches

---
+++ zsh/functions/gbr.zsh
@@ -0,0 +1,7 @@
+# gbr - checkout git branch
+gbr() {
+  local branches branch
+  branches=$(git branch -vv) &&
+  branch=$(echo "$branches" | fzf +m) &&
+  git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
+}
