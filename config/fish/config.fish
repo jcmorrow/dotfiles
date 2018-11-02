07ff5b35bc60b5402ac14d16269b2de8054c2850
Josh Morrow <josh@jcmorrow.com>
2018-11-02 11:10:21 +0900

    Small cleanup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,14 +3,14 @@ set -g -x PATH /Users/josh/.cargo/bin $PATH
 set -g -x PATH /usr/local/Cellar/rabbitmq/3.7.7_1/sbin/ $PATH
 set -x LESS '-iMSx4 -RSFX -e'
 
+alias __fzfcmd fzf
 alias g git
 alias python3.6 python
+alias sed 'sed -E'
 
 status --is-interactive; and source (rbenv init -|psub)
 eval (direnv hook fish)
 fish_vi_key_bindings
-
-alias __fzfcmd fzf
 
 function kill_server
   kill -9 (lsof -i tcp:$argv[1] -tc^Google)
