80976d3a28faadf39af3762b06fc4d1a9ab11ea2
Josh Morrow <josh@jcmorrow.com>
2024-01-25 07:13:28 -0800

    Add resume for telescope

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,10 @@
 local builtin = require("telescope.builtin")
+
 vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 vim.keymap.set("n", "<leader>F", builtin.git_files, {})
 vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>l", builtin.resume, {})
+
 vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
