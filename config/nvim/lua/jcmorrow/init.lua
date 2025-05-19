47e5125e526fbb300f65e551860378302afd3dc0
Josh Morrow <josh@jcmorrow.com>
2025-05-19 09:08:22 -0700

    Add light/dark mode detection

--- config/nvim/lua/jcmorrow/init.lua
+++ config/nvim/lua/jcmorrow/init.lua
@@ -4,6 +4,18 @@ require("jcmorrow.remap")
 vim.cmd([[
   autocmd BufReadPre * if getfsize(expand("%")) > 10000000 | syntax off | endif
 ]])
-vim.cmd([[colorscheme rose-pine-dawn]])
--- vim.cmd([[colorscheme rose-pine-moon]])
+
+local function is_macOS_dark_mode()
+  local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
+  local result = handle:read("*a")
+  handle:close()
+  return result and string.find(result, "Dark")
+end
+
+if is_macOS_dark_mode() then
+  vim.cmd([[colorscheme rose-pine-moon]])
+else
+  vim.cmd([[colorscheme rose-pine-dawn]])
+end
+
 vim.cmd("highlight! link MatchParen Cursor")
