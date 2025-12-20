9ec171015c94bc6d5f6e50c3a785c8d5e5180118
Josh Morrow <josh@jcmorrow.com>
2025-12-19 22:33:48 -0800

    Whew, disable double formatting

--- config/nvim/after/plugin/conform.lua
+++ config/nvim/after/plugin/conform.lua
@@ -1,10 +1,16 @@
 require("conform").setup({
+  log_level = vim.log.levels.TRACE,
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
   formatters_by_ft = {
     lua = { "stylua" },
     python = { "isort", "black" },
     rust = { "rustfmt", lsp_format = "fallback" },
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-    typescript = { "prettierd", "prettier", stop_after_first = true },
-    typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
   },
 })
--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -31,16 +31,16 @@ vim.lsp.config.ruff = {
   },
 }
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client and client:supports_method("textDocument/formatting") then
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        buffer = args.buf,
-        callback = function()
-          vim.lsp.buf.format({ async = false })
-        end,
-      })
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   callback = function(args)
+--     local client = vim.lsp.get_client_by_id(args.data.client_id)
+--     if client and client:supports_method("textDocument/formatting") then
+--       vim.api.nvim_create_autocmd("BufWritePre", {
+--         buffer = args.buf,
+--         callback = function()
+--           vim.lsp.buf.format({ async = false })
+--         end,
+--       })
+--     end
+--   end,
+-- })
