c4409a58df072919bb984c6d0128e3663fe9df1f
Josh Morrow <josh@jcmorrow.com>
2026-03-01 19:54:05 +0000

    Add border

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -47,3 +47,10 @@ vim.lsp.config.ruff = {
 --     end
 --   end,
 -- })
+
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or "rounded"
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
